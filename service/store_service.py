c3385f20ce0e3902a00d35d02f9022e6507c4b8f
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2022-05-26 15:23:23 +0900

    image storing process finish

--- service/store_service.py
+++ service/store_service.py
@@ -1,7 +1,45 @@
+import os
+
+import json
+import base64
+import time
+import datetime
+import uuid
+
 from database.data_handle.user_data_handle import find_user
 from database.data_handle.store_data_handle import find_store, update_store
 
-import json
+
+current_working_directory = os.getcwd()
+HOST = "http://localhost:8000"
+STORE_IMAGE_PATH = "/static/store_image/"
+MENU_IMAGE_PATH = "/static/menu_image/"
+
+
+def is_base64(string):
+    try:
+        base64.b64decode(string).decode('utf-8')
+        return True
+    except Exception as error:
+        # print(error)
+        return False
+
+
+def base64_decode(b64):
+    try:
+        return base64.b64decode(b64)
+    except Exception as error:
+        # print(error)
+        return None
+
+
+def get_current_unix_time():
+    now = datetime.datetime.now()
+    return time.mktime(now.timetuple())
+
+
+def file_name_generator(id):
+    return id + str(get_current_unix_time()).replace('.', '_') + str(uuid.uuid4())
 
 
 def get_store_info(store_info):
@@ -56,6 +94,40 @@ def set_store_info(store_info):
         return "idIsInvalidError"
 
     store_id = user['store_id']
+
+    store_photo_urls = store_photo['photo_urls']
+
+    # print(is_base64("aGVsbG8="))
+
+    for i in range(len(store_photo_urls)):
+        photo = store_photo_urls[i]
+        if is_base64(photo):
+            image_data = base64_decode(photo)
+            file_name = file_name_generator(id) + '.png'
+            try:
+                with open(current_working_directory + STORE_IMAGE_PATH + file_name, 'wb') as f:
+                    f.write(image_data)
+            except Exception as error:
+                print(error)
+                return "fileIOError"
+
+            store_photo_urls[i] = HOST + STORE_IMAGE_PATH + file_name
+
+    menu = menu_info['menu']
+
+    for i in range(len(menu_info)):
+        photo = menu[i]['photo']
+        if is_base64(photo):
+            image_data = base64_decode(photo)
+            file_name = file_name_generator(id) + '.png'
+            try:
+                with open(current_working_directory + MENU_IMAGE_PATH + file_name, 'wb') as f:
+                    f.write(image_data)
+            except Exception as error:
+                print(error)
+                return "fileIOError"
+
+            menu[i]['photo'] = HOST + MENU_IMAGE_PATH + file_name
 
     store_open_info = json.dumps(store_open_info)
     store_photo = json.dumps(store_photo)
